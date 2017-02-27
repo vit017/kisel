6947cea79fa2538bb5a070a9e79c6cd9a0325489
vit017 <johndoe@example.com>
2017-02-28 01:43:05 +0400

    contacts page

--- js/templ.js
+++ js/templ.js
@@ -2,4 +2,24 @@ $(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
+    if ($('#map-production').length) {
+        ymaps.ready(function () {
+            var coordsProduction = [56.828527, 53.173644]; //УР, Ижевск, Новоажимова, 12
+            var map = new ymaps.Map('map-production', {
+                center: coordsProduction,
+                zoom: 15
+            });
+
+            map.geoObjects.add(
+                new ymaps.Placemark(
+                    coordsProduction,
+                    {iconCaption: 'улица Новоажимова, 12'},
+                    {preset: "islands#blueCircleDotIconWithCaption"}
+                )
+            );
+
+            map.controls.destroy()
+        });
+    }
+
 });
