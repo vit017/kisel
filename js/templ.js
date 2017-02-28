b413a18a2a0bf975a4b7d197ed293358c1e3e89e
vit017 <johndoe@example.com>
2017-02-28 04:46:19 +0400

    catalog detail add compare top panel

--- js/templ.js
+++ js/templ.js
@@ -1,23 +1,50 @@
 $(function () {
 
+
+    /* catalog detail */
+    $('.panel-compare .close').click(function() {
+        $(this).closest('.panel-compare').css('opacity', 0);
+    });
+
+    $('.cart .compare').click(function () {
+        var $this = $(this),
+            $parent = $this.closest('.page-catalog-detail'),
+            $panel = $('.panel-compare'),
+            $panelImg = $panel.find('.product-img').find('img'),
+            $panelProduct = $panel.find('.product-title').find('span'),
+            title = $parent.find('h1.title').text(),
+            imgSrc = $parent.find('.img-detail').find('img').attr('src');
+
+        console.log($panel)
+        console.log($panelImg)
+        console.log($panelProduct)
+        console.log(title)
+        console.log(imgSrc)
+
+        $panelImg.attr('src', imgSrc);
+        $panelImg.attr('alt', title);
+
+        $panelProduct.text(title);
+        $panel.css('opacity', 1);
+    });
+
     /* catalog compare */
     /*
-    $('.ico-del').click(function() {
-        var $this = $(this);
-        var $carousel = $this.closest('.carousel');
-        var $item = $('[data-id='+$this.closest('.item-catalog').data('id')+']');
+     $('.ico-del').click(function() {
+     var $this = $(this);
+     var $carousel = $this.closest('.carousel');
+     var $item = $('[data-id='+$this.closest('.item-catalog').data('id')+']');
 
-        //$item.hide();
-        //$carousel.carousel('next');
-        //$item.remove();
+     //$item.hide();
+     //$carousel.carousel('next');
+     //$item.remove();
 
-        !$carousel.find('.item-catalog').length && $carousel.remove();
-    });
-    */
+     !$carousel.find('.item-catalog').length && $carousel.remove();
+     });
+     */
 
     $('[data-toggle="tooltip"]').tooltip();
     /* catalog compare */
-
 
 
     /* contacts page */
