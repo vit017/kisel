390644f2e5d87f118c5d7d4feb8c8e79157ad4e8
vit017 <johndoe@example.com>
2017-03-03 05:28:59 +0400

    catalog page: compare btn

--- js/templ.js
+++ js/templ.js
@@ -1,15 +1,58 @@
 $(function () {
 
+    /**
+     * check fix buttons (arrow-up, callback) on window scroll event
+     * @type {*}
+     */
     var $parent = $('.btns-fix'),
         $btnArrow = $parent.find('.arrow-up'),
-        $btnCallback = $parent.find('.callback'),
         btnOpacity = getComputedStyle($btnArrow[0]).opacity,
+        $btnCallback = $parent.find('.callback'),
         $footer = $('#footer'),
         maxScroll = $footer.offset().top;
 
     $btnArrow[0].addEventListener('click', scrollWindow.bind(null, $btnArrow));
     window.addEventListener('scroll', checkFixBtns.bind(null, maxScroll, [$btnArrow, $btnCallback], btnOpacity));
+
+
+    /**
+     * show tooltip - add product to compare list. catalog page
+     */
+    if ($('.item .img-catalog').length) {
+        $('.item .img-catalog').mouseenter(function () {
+            $(this).parent().find('.img-compare').show();
+        });
+        $('.item .img-catalog').mouseleave(function (ev) {
+            $(this).parent().find('.img-compare').hide();
+        });
+        $('.img-compare').click(function () {
+            var $objPamel = getComparePanel(),
+                $parent = $(this).closest('.item'),
+                title = $parent.find('.text-catalog .product-title').text(),
+                imgSrc = $parent.find('.img-product').attr('src');
+
+            $objPamel.img.attr('src', imgSrc);
+            $objPamel.img.attr('alt', title);
+
+            $objPamel.product.text(title);
+            $objPamel.panel.css('z-index', 999);
+            $objPamel.panel.css('opacity', 1);
+
+            $(this).hide();
+        });
+    }
+    /* catalog */
+
+
 });
+
+function getComparePanel() {
+    var $panel = $('#panel-compare'),
+        $panelImg = $panel.find('.product-img').find('img'),
+        $panelProduct = $panel.find('.product-title').find('span');
+
+    return {'panel': $panel, 'img': $panelImg, 'product': $panelProduct};
+}
 
 function scrollWindow($btnArrow) {
     window.removeEventListener('scroll', checkFixBtns);
